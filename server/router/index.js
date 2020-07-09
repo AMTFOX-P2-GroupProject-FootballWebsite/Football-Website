985539d2dc2a87dd73000f6961027a428d657b47
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-07-09 22:48:28 +0700

    commit dulu

--- server/router/index.js
+++ server/router/index.js
@@ -3,6 +3,6 @@ const homeRoute = require('./homeRoute')
 const footballRoute = require('./footballRoute')
 
 route.use('/',homeRoute)
-route.use('/football',footballRoute)
+route.use('/football-video',footballRoute)
 
 module.exports = route
