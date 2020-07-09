abae47ef06fb437c9d34bf5768af03b7520dc70a
kvngrldy <kevingrldy@gmail.com>
2020-07-10 03:22:33 +0700

    news-api done

---
+++ server/router/newsRoute.js
@@ -0,0 +1,9 @@
+const route = require('express').Router()
+const newsController = require('../controllers/newsController')
+const authentication = require('../middleware/authentication')
+
+//route.use(authentication)
+
+route.get('/premierleague', newsController.list)
+
+module.exports = route
