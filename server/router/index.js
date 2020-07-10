9cc7e0668036a54d55bbdd331c4155ceeadfdb38
Alfonso-270702 <tebaksiapa27@gmail.com>
2020-07-10 11:54:37 +0700

    sudah ada Highlights

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,7 @@
-
-
 const route = require('express').Router()
 const homeRoute = require('./homeRoute')
 const footballRoute = require('./footballRoute')
 const newsRoute = require('./newsRoute')
-
 
 route.use('/',homeRoute)
 route.use('/football-video',footballRoute)
